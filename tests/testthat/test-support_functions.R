76a3c77917c22f9f71a0da70198085439b90f771
jclutton <jonathan.clutton@sickkids.ca>
2026-02-17 17:21:01 -0500

    Improving testing

--- tests/testthat/test-support_functions.R
+++ tests/testthat/test-support_functions.R
@@ -1,4 +1,31 @@
-test_that("clean_file looks for spreadsheets ", {
-  file <- file.path("dir","testing.pdf")
-  expect_error(clean_file(file))
+test_that("clean_file finds missing columns", {
+  file <- system.file("extdata", "sample_swan.csv", package = "sfsScorer")
+  df <- rio::import(file) |>
+    select(-swan1)
+  expect_error(clean_file(df))
+})
+
+
+test_that("clean_file finds impossible values", {
+  file <- system.file("extdata", "sample_swan.csv", package = "sfsScorer")
+  df <- rio::import(file) |>
+    mutate(swan1 = 10)
+  expect_error(clean_file(df))
+})
+
+test_that("mkvars works properly", {
+  vars <- mkvars(1, 18, root = 'swan')
+  expect_equal(vars, paste0('swan',seq(1, 18, by = 1)))
+})
+
+test_that("mkpro works properly", {
+  df <- data.frame(swan1 = c(1, 2, 0, NA),
+                   swan2 = c(-2, NA, 1, NA),
+                   swan3 = c(1, 3, -1, 2),
+                   swan4 = c(1, 2, 0, 1))
+  check <- data.frame(mkpro(maxmiss = 1, dat = df, a = 1, b = 4))
+  expect <- data.frame(swan_tot = c(1, 7, 0, NA),
+                       swan_miss = c(0, 1, 0, 2),
+                       swan_pro = c(1, 9 + 1/3, 0, NA))
+  expect_equal(check, expect)
 })
